67bd96e9d51f1d45ceeac675ea0e958a97a7e6b6
earthtojake <jakehenryfitz@gmail.com>
2022-03-13 00:13:34 +1100

    fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,74 +16,81 @@ import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { FITZCOIN_ADDRESS } from "../constants";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
   return (
-    <ChakraProvider theme={theme}>
-      <div className="gradient stack1"></div>
-      <div className="gradient stack2"></div>
-      <div className="gradient stack3"></div>
-      <Center p={16}>
-        <Stack alignItems={"center"} maxWidth={420}>
-          <Heading textAlign="center" size="4xl">
-            FiTzCoIn
-          </Heading>
-          <Image
-            sx={{
-              transform: "rotateY(45deg)",
-              animation: "rotateAnimation 3.0s linear infinite",
-            }}
-            src="/fitzcoin.png"
-            width={180}
-            height={180}
-          />
-          <Heading textAlign="center">The UlTiMaTe ShiTCoiN</Heading>
-          <Heading fontSize="md" opacity={0.7}>
-            Total Supply: 100 FitzCoin
-          </Heading>
-          <Center pb={6}>
-            <Button
-              size="sm"
-              aria-label="Buy"
-              mr={2}
-              colorScheme={
-                !asPath.startsWith("/fitzenomics") ? "green" : undefined
-              }
-            >
-              <NextLink href="/" passHref>
-                <LinkOverlay>Buy</LinkOverlay>
-              </NextLink>
-            </Button>
-            <Button
-              size="sm"
-              aria-label="View Contract"
-              mr={2}
-              colorScheme={
-                asPath.startsWith("/fitzenomics") ? "green" : undefined
-              }
-            >
-              <NextLink href="/fitzenomics" passHref>
-                <LinkOverlay>Fitzenomics</LinkOverlay>
-              </NextLink>
-            </Button>
-            <Button
-              size="sm"
-              aria-label="View Contract"
-              rightIcon={<ExternalLinkIcon />}
-            >
-              <LinkOverlay
-                href={`https://etherscan.io/address/${FITZCOIN_ADDRESS}`}
-                target="_blank"
+    <>
+      <Head>
+        <title>FiTzCoIn, the UlTimAtE ShiTcOiN</title>
+        <meta name="description" content="Buy FITZ to earn FITZ" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <div className="gradient stack1"></div>
+        <div className="gradient stack2"></div>
+        <div className="gradient stack3"></div>
+        <Center p={16}>
+          <Stack alignItems={"center"} maxWidth={420}>
+            <Heading textAlign="center" size="4xl">
+              FiTzCoIn
+            </Heading>
+            <Image
+              sx={{
+                transform: "rotateY(45deg)",
+                animation: "rotateAnimation 3.0s linear infinite",
+              }}
+              src="/fitzcoin.png"
+              width={180}
+              height={180}
+            />
+            <Heading textAlign="center">The Ultimate Shitcoin</Heading>
+            <Heading fontSize="md" opacity={0.7}>
+              Total Supply: 100 FITZ
+            </Heading>
+            <Center pb={6}>
+              <Button
+                size="sm"
+                aria-label="Buy"
+                mr={2}
+                colorScheme={
+                  !asPath.startsWith("/fitzenomics") ? "green" : undefined
+                }
               >
-                View Contract
-              </LinkOverlay>
-            </Button>
-          </Center>
-          <Component {...pageProps} />
-        </Stack>
-      </Center>
-    </ChakraProvider>
+                <NextLink href="/" passHref>
+                  <LinkOverlay>Buy</LinkOverlay>
+                </NextLink>
+              </Button>
+              <Button
+                size="sm"
+                aria-label="View Contract"
+                mr={2}
+                colorScheme={
+                  asPath.startsWith("/fitzenomics") ? "green" : undefined
+                }
+              >
+                <NextLink href="/fitzenomics" passHref>
+                  <LinkOverlay>Fitzenomics</LinkOverlay>
+                </NextLink>
+              </Button>
+              <Button
+                size="sm"
+                aria-label="View Contract"
+                rightIcon={<ExternalLinkIcon />}
+              >
+                <LinkOverlay
+                  href={`https://etherscan.io/address/${FITZCOIN_ADDRESS}`}
+                  target="_blank"
+                >
+                  View Contract
+                </LinkOverlay>
+              </Button>
+            </Center>
+            <Component {...pageProps} />
+          </Stack>
+        </Center>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/fitzenomics.tsx
+++ pages/fitzenomics.tsx
@@ -7,10 +7,9 @@ const Fitzenomics: NextPage = () => {
     <Stack textAlign={"center"}>
       <Heading size="md">Total Supply</Heading>
       <Text fontSize="md">
-        There is a total supply of 100 FITZ representing 100 years of Fitz's
-        life. The initial supply is 60 FITZ starting on his 60th birthday. The
-        remaining 40 FITZ will be emitted over the next 40 years. You can view
-        the FITZ ERC-20 contract{" "}
+        {
+          "There is a total supply of 100 FITZ representing 100 years of Fitz's life. The initial supply is 60 FITZ starting on his 60th birthday. The remaining 40 FITZ will be emitted over the next 40 years. You can view the FITZ ERC-20 contract "
+        }
         <Link
           color="blue"
           href={`https://etherscan.io/address/${FITZCOIN_ADDRESS}`}
@@ -23,7 +22,7 @@ const Fitzenomics: NextPage = () => {
       <Heading pt={4} size="md">
         Emissions
       </Heading>
-      <Text fontSize="md">THe remaining 40 FITZ</Text>
+      <Text fontSize="md">The remaining 40 FITZ is </Text>
     </Stack>
   );
 };
